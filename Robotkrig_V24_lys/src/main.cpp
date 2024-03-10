4b843a294578567b308cdc88b02eb9ed3d52f13d
eskilrl <eskilrl@yahoo.com>
2024-03-10 12:27:33 +0100

    trash lys kode

--- Robotkrig_V24_lys/src/main.cpp
+++ Robotkrig_V24_lys/src/main.cpp
@@ -9,8 +9,8 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numOfLeds, dinPin, NEO_GRB + NEO_KH
 // Color takes RGB values, from 0,0,0 up to 255,255,255
 // e.g. White = (255,255,255), Red = (255,0,0);
 int red = 255; 		//Value from 0(led-off) to 255(). 
-int green = 00; 
-int blue = 0;
+int green = 255; 
+int blue = 255;
 
 
 void setup() {
@@ -21,8 +21,13 @@ void setup() {
 void loop() {
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
   for(int i=0;i<numOfLeds;i++){
-    pixels.setPixelColor(i, pixels.Color(red,green,blue)); 
+    if(i%2){
+      pixels.setPixelColor(i, pixels.Color(0,0,blue)); 
+    } else {
+      pixels.setPixelColor(i, pixels.Color(red,green,blue)); 
+    }
     pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(1000);   // Delay for a period of time to change the next led
+
   }
+  delay(100);   // Delay for a period of time to change the next led
 }
